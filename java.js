4d55e260b74e9af5b3108f901a1063d7c6822f24
CStank89 <67664834+CStank89@users.noreply.github.com>
2020-07-30 23:30:36 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+
+var hello = prompt(
+    "Hello Tobias!  The Web35 class contains 157 students"
+);
+   
+
+    
+var number = prompt(
+    "I have chosen a random number between 1 and 157 " +  "It is " + (Math.random() * 157) + " I rock!"
+);
+
+
+
+
+var submissions = prompt(
+    "How many challenge submissions were correct?");
+    console.log(submissions)
+
+
+var correct = submissions
+
+ var last = prompt(
+    "I have computed a random number between 1 and "+ correct + " it is " + (Math.random() * correct +"." + " Alphabetically sort the names of those who submitted correct answers and select the person at the number I chose as the winner. Thank you!”")
+ );
+
